7f3a429ecf695b9b00a7fedeee0ad7f9e0a64037
108yen <kazuking.1911@gmail.com>
2024-04-17 17:18:19 +0900

    feat(theme): added custom theme

---
+++ src/theme/index.ts
@@ -0,0 +1,18 @@
+"use client";
+
+import { UsageTheme, extendConfig, extendTheme } from "@yamada-ui/react";
+import components from "./components";
+import { globalStyle, resetStyle, layerStyles, textStyles } from "./styles";
+import tokens from "./tokens";
+import { customConfig } from "./config";
+
+export const customTheme: UsageTheme = {
+  styles: { globalStyle, resetStyle, layerStyles, textStyles },
+  components,
+  ...tokens,
+};
+
+export const theme = extendTheme(customTheme)();
+export const config = extendConfig(customConfig);
+
+export default theme;
