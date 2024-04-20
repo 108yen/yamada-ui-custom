0e1e6b42e90f623dfb7f29cd32915fd1aa6aab16
108yen <kazuking.1911@gmail.com>
2024-04-20 19:08:00 +0900

    feat(app): added `thtme` switching with`onClick`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,79 @@
+"use client"
 import { Header } from "@/component/layouts/header"
-import { Button, Center, Container, Heading } from "@yamada-ui/react"
+import { Button, Center, Container, Heading, useTheme } from "@yamada-ui/react"
 
 export default function Home() {
+  const { changeThemeScheme } = useTheme()
+
   return (
     <>
       <Header />
       <Center>
         <Container size="sm" variant="with-border-dotted" layerStyle="serie">
-          <Button colorScheme="frieren">frieren</Button>
-          <Button colorScheme="fern">fern</Button>
-          <Button colorScheme="stark">stark</Button>
-          <Button colorScheme="himmel">himmel</Button>
-          <Button colorScheme="heiter">heiter</Button>
-          <Button colorScheme="eisen">eisen</Button>
-          <Button colorScheme="flamme">flamme</Button>
-          <Button colorScheme="serie">serie</Button>
+          <Button
+            colorScheme="frieren"
+            onClick={() => {
+              changeThemeScheme("frieren")
+            }}
+          >
+            frieren
+          </Button>
+          <Button
+            colorScheme="fern"
+            onClick={() => {
+              changeThemeScheme("fern")
+            }}
+          >
+            fern
+          </Button>
+          <Button
+            colorScheme="stark"
+            onClick={() => {
+              changeThemeScheme("stark")
+            }}
+          >
+            stark
+          </Button>
+          <Button
+            colorScheme="himmel"
+            onClick={() => {
+              changeThemeScheme("himmel")
+            }}
+          >
+            himmel
+          </Button>
+          <Button
+            colorScheme="heiter"
+            onClick={() => {
+              changeThemeScheme("heiter")
+            }}
+          >
+            heiter
+          </Button>
+          <Button
+            colorScheme="eisen"
+            onClick={() => {
+              changeThemeScheme("eisen")
+            }}
+          >
+            eisen
+          </Button>
+          <Button
+            colorScheme="flamme"
+            onClick={() => {
+              changeThemeScheme("flamme")
+            }}
+          >
+            flamme
+          </Button>
+          <Button
+            colorScheme="serie"
+            onClick={() => {
+              changeThemeScheme("serie")
+            }}
+          >
+            serie
+          </Button>
           <Heading textStyle="gradient">
             ありがとうフリーレン。君のおかげで最後にとても楽しい冒険ができた。綺麗だ。
           </Heading>
